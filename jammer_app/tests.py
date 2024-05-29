8745300e9a9051d5d1d742d83bf2d5e86c8447e6
EricNdivo <j.ericndivo@gmail.com>
2024-05-29 16:42:11 +0300

    tests(FF)

--- jammer_app/tests.py
+++ jammer_app/tests.py
@@ -16,3 +16,27 @@ class TestJammingView(TestCase):
         response = self.client.post('/start_jamming/', {'interface': 'wlan0'})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(responsejson(), {'status': 'jamming already in progress'})
+
+    def test_stop_jamming_no_jamming_in_progress(self):
+        response = self.client.post('/stop_jamming/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'status': 'No jamming in progress'})
+
+    @patch('jammer_app.views.jamming', True)
+    @patch('jammer_app.views.jam_thread')
+    def test_stop_jamming(self, mock_jam_thread):
+        mock_jam_thread.join.return_value = None
+        response = self.client.post('/stop_jamming/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'status': 'jaming stopped'})
+
+    def test_start_jamming_invalid_method(self):
+        response = self.client.get('/stop_jamming')
+        self.assertEqual(response.status_code, 405)
+        self.assertEqual(response.json(), {'error': 'Method not allowed'})
+
+    def test_stop_jamming_invalid_method(self):
+        response = self.client.get('/stop_jamming')
+        self.assertEqual(response.status_code, 405)
+        self.assertEqual(response.json(), {'error': 'Method not allowed'})
+
